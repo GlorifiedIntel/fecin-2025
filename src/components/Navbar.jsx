fdf631ce48b427496e15523303ec57aa57ea7ab6
KEVIN CROSS MINCHAKPU <165849310+GlorifiedIntel@users.noreply.github.com>
2024-12-25 15:02:38 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,22 @@
 import React from "react";
-import "./Navbar.css";
+import "./Navbar.css"; // Import the CSS file
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container">
         <h1 className="logo-text">FECIN</h1>
-        <ul className="navbar-list">
-          <li><a href="/">Home</a></li>
-
-          {/* About Dropdown */}
-          <li className="dropdown">
-            <a href="#" className="dropdown-link">About</a>
-            <ul className="dropdown-menu">
-              <li><a href="/what-we-do">What We Do</a></li>
-              <li><a href="/where-we-serve">Where We Serve</a></li>
-              <li><a href="/contact-us">Contact Us</a></li>
-            </ul>
-          </li>
-
-          <li><a href="/leadership">Leadership</a></li>
-          <li><a href="/events">Events</a></li>
-          <li><a href="/prayer-request">Prayer Requests</a></li>
-          <li><a href="/donate">Donate</a></li>
-        </ul>
+        <div className="nav-links">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about-us">About Us</a></li>
+            <li><a href="/leadership">Leadership</a></li>
+            <li><a href="/events">Events</a></li>
+            <li><a href="/prayer-requests">Prayer Requests</a></li>
+            <li><a href="/donate">Donate</a></li>
+            <li><a href="/login" className="login-button">Login</a></li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
